6e90f69dcd7b4571013aa62c43086bd3dfcdd744
nataliaptzk <nataliaptzk@gmail.com>
2020-03-03 10:00:47 +0000

    WIP json parsing

--- GRD Client Project/Assets/Scripts/MG #3 Quiz/Quiz.cs
+++ GRD Client Project/Assets/Scripts/MG #3 Quiz/Quiz.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UnityEngine;
 using TMPro;
 
@@ -8,10 +10,13 @@ public class Quiz : Level
 {
     [SerializeField] private TextMeshProUGUI _questionField;
     [SerializeField] private GameObject _answerDropArea;
+
     [SerializeField] private List<GameObject> answerObjects = new List<GameObject>();
-    [SerializeField] private List<Question> _questions = new List<Question>();
+
+    //[SerializeField] private List<Question> _questions = new List<Question>();
     private int _currentQuestion;
-    public List<Question> Questions => _questions;
+
+    //public List<Question> Questions => _questions;
     public int CurrentQuestion => _currentQuestion;
 
     private void Awake()
@@ -22,36 +27,57 @@ public class Quiz : Level
 
     private void Start()
     {
+        LoadJson();
         StartCoroutine(_timer.Countdown(SessionManager.CurrentDifficulty.duration * _miniGameBaseTime));
     }
 
     private void FirstQuestion()
     {
-        _questionField.text = _questions[_currentQuestion].question;
+        /*_questionField.text = _questions[_currentQuestion].question;
 
         for (int i = 0; i < _questions[_currentQuestion].answers.Count; i++)
         {
             answerObjects[i].gameObject.transform.GetChild(0).transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = _questions[_currentQuestion].answers[i];
-        }
+        }*/
     }
 
     public void NextQuestion()
     {
-        _currentQuestion++;
+/*        _currentQuestion++;
         _currentQuestion = Mathf.Clamp(_currentQuestion, 0, _questions.Count - 1);
         _questionField.text = _questions[_currentQuestion].question;
 
         for (int i = 0; i < _questions[_currentQuestion].answers.Count; i++)
         {
             answerObjects[i].gameObject.transform.GetChild(0).transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = _questions[_currentQuestion].answers[i];
+        }*/
+    }
+
+   
+
+    [Serializable]
+    public class RootObject
+    { [Serializable]
+        public class QuestionObject
+        {
+            public string question;
+            public string answer1;
+            public string answer2;
+            public string answer3;
+            public string difficulty;
+            public string include;
+        }
+        public List<QuestionObject> questionObjects;
+    }
+
+    [ContextMenu("asdf")]
+    public void LoadJson()
+    {
+        using (StreamReader r = new StreamReader("Assets/Resources/quizdata.json"))
+        {
+            string json = r.ReadToEnd();
+            JsonUtility.FromJson<RootObject>(json);
+            Debug.Log(JsonUtility.FromJson<RootObject>(json).questionObjects.First());
         }
     }
-}
-
-[Serializable]
-public class Question
-{
-    public string question;
-    public List<string> answers = new List<string>();
-    public int correctAnswer;
 }
