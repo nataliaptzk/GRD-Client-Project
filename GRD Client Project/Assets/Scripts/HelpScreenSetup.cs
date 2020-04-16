d5f03ac9d800e276a8a455182c2aee58bf578670
nataliaptzk <nataliaptzk@gmail.com>
2020-04-16 22:35:45 +0100

    Help screen for easy

--- GRD Client Project/Assets/Scripts/HelpScreenSetup.cs
+++ GRD Client Project/Assets/Scripts/HelpScreenSetup.cs
@@ -16,15 +16,25 @@ public class HelpScreenSetup : MonoBehaviour
 
     private void HelpSetup()
     {
-        if (SessionManager.CurrentDifficulty.name == "easy" || SessionManager.CurrentDifficulty.name == "normal")
+        if (SessionManager.CurrentDifficulty.name == "easy")
         {
             _helps[0].SetActive(true);
             _helps[1].SetActive(false);
+            _helps[2].SetActive(false);
+        }
+        else if (SessionManager.CurrentDifficulty.name == "normal")
+        {
+            _helps[0].SetActive(false);
+            _helps[1].SetActive(true);
+            _helps[2].SetActive(false);
+
         }
         else if (SessionManager.CurrentDifficulty.name == "hard")
         {
             _helps[0].SetActive(false);
-            _helps[1].SetActive(true);
+            _helps[1].SetActive(false);
+            _helps[2].SetActive(true);
+
         }
     }
 
