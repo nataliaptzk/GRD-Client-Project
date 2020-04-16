f60f5ee1e923231945bf151e8fd599e3e14cfd94
nataliaptzk <nataliaptzk@gmail.com>
2020-04-16 22:27:35 +0100

    Follow Camera in investigation game

---
+++ GRD Client Project/Assets/FollowCameraInvestigation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCameraInvestigation : MonoBehaviour
+{
+    [SerializeField] private Transform _itemToFollow;
+    [SerializeField] private Vector3 _offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, _itemToFollow.position + _offset, 1f);
+    }
+}
